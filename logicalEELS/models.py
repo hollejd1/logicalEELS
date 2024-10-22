Fixed Spectra scaling causing training errors

logicalEELS/models.py
@@ -174,7 +174,7 @@ class dualVAE(keras.Model):
             Y_reconstruction = self.decoder(zy)
             reconstruction_loss = tf.reduce_sum(keras.losses.mae(y, Y_reconstruction), axis=1)
 
-            convergence_loss = tf.reduce_sum(keras.losses.mae(tf.expand_dims(zy_mean, -1), tf.expand_dims(zx, -1)), axis=1)
+            convergence_loss = tf.reduce_sum(keras.losses.mse(tf.expand_dims(zy_mean, -1), tf.expand_dims(zx, -1)), axis=1)
 
             kl_loss = -0.5 * (1 + zy_log_var - tf.square(zy_mean) - tf.exp(zy_log_var))
             kl_loss = tf.reduce_sum(kl_loss, axis=1)