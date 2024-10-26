e308e2bc3391230db0be745fd3dfbc5ef260410a
Darian Smalley <darian.smalley@gmail.com>
2024-10-26 13:45:34 -0400

    Update models.py

--- logicalEELS/models.py
+++ logicalEELS/models.py
@@ -147,7 +147,7 @@ class dualVAE(keras.Model):
 
         # self.inputShape = self.X_encoder.layers[0].input_shape[0]
         # self.latentDims = self.decoder.layers[0].input_shape[0]
-        print(self.X_encoder.layers[0].get_config(), tf.shape(self.X_encoder.layers[0], self.X_encoder.layers[0].shape))
+        print(self.X_encoder.layers[0].get_config(), tf.shape(self.X_encoder.layers[0]))
         self.inputShape = self.X_encoder.layers[0].get_config()['batch_size'][1]
         self.latentDims = self.decoder.layers[0].get_config()['batch_size'][1]
     
